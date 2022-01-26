d2168e701ea9a2180f7ee16324f3719f8a75200f
youccode <woochan226544@snu.ac.kr>
2022-01-27 01:09:16 +0900

    Starting the studing the Javascript watching the clone coding

---
+++ Clone_Coding/script.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 2;
+
+const myName = "ccode";
+const veryLingVariableName = 0;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
