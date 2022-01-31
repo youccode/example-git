f6612179a8a318d6d0cfb3fbbbcd17002820e0f5
Navier <woochan226544@snu.ac.kr>
2022-01-31 18:17:42 +0900

    Doing Stuffs in Grandfather's house

--- Clone_Coding/script.js
+++ Clone_Coding/script.js
@@ -1,10 +1,18 @@
 const a = 5;
 const b = 2;
 
-const myName = "ccode";
+let myName = "ccode";
 const veryLingVariableName = 0;
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello " + myName);
+console.log("hello " + myName); /* const와 let의 차이가 중요하지 않은 부분 */
+
+myName = "Woochan"; /* const와 let의 차이가 중요한 부분 */
+
+console.log("Your New Name is " + myName);
+
+const amIFat = true;
+
+console.log(amIFat);
