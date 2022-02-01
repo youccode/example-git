13617be32529f3c840dde27d465fdbaeb0cfeccb
Navier <woochan226544@snu.ac.kr>
2022-02-01 12:58:37 +0900

    commit in the 2nd day in the Grandfather's house

--- Clone_Coding/script.js
+++ Clone_Coding/script.js
@@ -1,18 +1,58 @@
-const a = 5;
-const b = 2;
+// const a = 5;
+// const b = 2;
+// let something; /* undefined data*/
 
-let myName = "ccode";
-const veryLingVariableName = 0;
+// let myName = "ccode";
+// const veryLingVariableName = 0;
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName); /* const와 let의 차이가 중요하지 않은 부분 */
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName); /* const와 let의 차이가 중요하지 않은 부분 */
 
-myName = "Woochan"; /* const와 let의 차이가 중요한 부분 */
+// myName = "Woochan"; /* const와 let의 차이가 중요한 부분 */
 
-console.log("Your New Name is " + myName);
+// console.log("Your New Name is " + myName);
 
-const amIFat = true;
+// const amIFat = true;
 
-console.log(amIFat);
+// console.log(amIFat);
+// console.log(something);
+
+/* Part for the array */
+
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sund";
+
+// const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
+
+// console.log(daysOfWeek);
+/* nou using array, we can just make the bunches of strings which is useless */
+
+// const daysOfWeek_array = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+// // Get Item from Array
+// console.log(daysOfWeek_array);
+
+// // Add one more Item to the Array
+
+// daysOfWeek_array.push("none");
+
+// console.log(daysOfWeek_array);
+
+// How to organize the "Object"
+
+const player = {
+  name: "woochan",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+player.lastname = "Im";
+console.log(player.lastname);
