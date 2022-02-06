e769a6cfb3f914d63a9ac7eb20b4010bdff5e9d1
youccode <woochan226544@snu.ac.kr>
2022-02-06 22:29:18 +0900

    sdf

--- Clone_Coding/script.js
+++ Clone_Coding/script.js
@@ -1,9 +1,33 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-console.dir(h1);
+const HIDDEN_CLASSNAME = "hidden"; // string만 저장된 변수는 convention으로 다음과 같이 대문자로 표기
+const USERNAME_KEY = "username";
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const nameOfUser = loginInput.value; // value가 Input Block 안에 들어간 내용을 의미함.
+  localStorage.setItem(USERNAME_KEY, nameOfUser);
+
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreeting(nameOfUser);
 }
 
-h1.addEventListener("click", handleTitleClick);
+function paintGreeting(username) {
+  greeting.innerText = `Hello! ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //show the greetings
+  paintGreeting(savedUsername);
+}
