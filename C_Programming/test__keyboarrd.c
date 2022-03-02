e2b61bb5513f5272970407530838dac51561a058
Navier <woochan226544@snu.ac.kr>
2022-03-02 20:50:35 +0900

    Studing Java day 2

---
+++ C_Programming/test__keyboarrd.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+    printf("뭔가 희미하게 울림의 소리가 들리는데 괜찮을라나\n");
+    
+}
---
+++ Java/dataType.java
@@ -0,0 +1,6 @@
+public class dataType {
+    public static void main(String[] args) {
+        String a = "Hello Java";
+        System.out.println(a.charAt(6).getClass());
+    }
+}
